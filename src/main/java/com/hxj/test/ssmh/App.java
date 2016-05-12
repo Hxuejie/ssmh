d5801ab3135ed360cda4d6a80ad245f2791657d7
Hxuejie <hxuejie@126.com>
2016-05-12 08:38:20 +0800

    create java project

---
+++ src/main/java/com/hxj/test/ssmh/App.java
@@ -0,0 +1,13 @@
+package com.hxj.test.ssmh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
